50e1160892cb9c44285e603a3ad946a1c96f2c2b
Marcello <marcelloaltermundi@gmail.com>
2024-01-18 12:08:07 -0300

    iniciando exercicios

--- src/main/java/com/trybe/java/escolainteligente/App.java
+++ src/main/java/com/trybe/java/escolainteligente/App.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class App {
 
   /**
-   * Método principal.
+   * Método principal...
    */
   public static void main(String[] args) {
     System.out.println("Boas vindas a Escola Inteligente!");
